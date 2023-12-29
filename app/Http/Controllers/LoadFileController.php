9aebd395880649ef66f2b6ca8bcb7d82b7bdb9d9
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2023-12-30 00:12:36 +0800

    .

--- app/Http/Controllers/LoadFileController.php
+++ app/Http/Controllers/LoadFileController.php
@@ -8,8 +8,7 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class LoadFileController extends Controller
 {
-<<<<<<< HEAD
-   public function upload(Request $request) {
+    public function upload(Request $request) {
         $message = "";
         $textInput1 = $request->input('workName');
         $selectValue1 = $request->input('typeWork');
@@ -45,47 +44,6 @@ class LoadFileController extends Controller
                         }
                     }
                     $ulpoadFiles[] = $file;
-=======
-   public function upload(Request $request)
-   {
-       $textInput1 = $request->input('workName');
-       $selectValue1 = $request->input('typeWork');
-       $selectValue2 = $request->input('subAreaWork');
-       $textInput2 = $request->input('workLink');
-
-       if (!empty($textInput1) && !empty($selectValue1) && !empty($selectValue2) && !empty($textInput2)){
-           $destinationPath = public_path('uploads');
-           $ulpoadFiles = [];
-
-           for ($i = 1; $i < 5; $i++) {
-               $fileName = "uploadedFile$i";
-
-               if ($request->hasFile($fileName)){
-                  $file = $request->file($fileName);
-                  $file->move($destinationPath, $file->getClientOriginalName());
-                  $ulpoadFiles[] = $file;
-               }
-           }
-
-           // Преобразование PDF в TXT с помощью Python скрипта
-           if ($request->hasFile('uploadedFile1')) {
-               $pdfFilePath = public_path('uploads/' . $request->file('uploadedFile1')->getClientOriginalName());
-               $txtFilePath = public_path('uploads/translated.txt');
-
-               $process = new Process(['python', storage_path('Scripts/TranslateScriptPDFtoTXT.py'), $pdfFilePath, $txtFilePath]);
-
-               try {
-                $process->mustRun();
-             
-                if ($process->isSuccessful()) {
-                    // Скрипт успешно выполнился
-                    $output = $process->getOutput();
-                    echo "Скрипт успешно выполнился, вывод:\n{$output}\n";
-                } else {
-                    // Скрипт завершился с ошибкой
-                    $errorOutput = $process->getErrorOutput();
-                    echo "Скрипт завершился с ошибкой, вывод ошибки:\n{$errorOutput}\n";
->>>>>>> 202d7516400e45d8824417ddc4d51ea2b69cb098
                 }
              } catch (ProcessFailedException $exception) {
                 // Процесс не удалось запустить
@@ -93,7 +51,6 @@ class LoadFileController extends Controller
              }
            }
 
-<<<<<<< HEAD
             foreach ($ulpoadFiles as $file) {
                 $fileName = $file->getClientOriginalName();
                 $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
@@ -154,15 +111,5 @@ class LoadFileController extends Controller
             return $message;
         }
     }
-=======
-           if (!empty($ulpoadFiles) && isset($ulpoadFiles[1])){
-            return "$textInput1, $selectValue1, $selectValue2, $textInput2, файл $ulpoadFiles[0] успешно загружен в $destinationPath и файл файл $ulpoadFiles[1] успешно загружен в $destinationPath";
-         } else {
-            return "Файлы не были загружены";
-         }
-       } else {
-           return "$textInput1, $selectValue1, $selectValue2, $textInput2, Пожалуйста, заполните пустые поля";
-       }
-   }
->>>>>>> 202d7516400e45d8824417ddc4d51ea2b69cb098
 }
+
