99fdb55315bfbf216c30d834bb4b2882951bbc00
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2023-12-30 11:52:17 +0800

    изменен скрипт

---
+++ public/scripts/TranslateScriptPDFtoTXT.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from pdfminer.high_level import extract_text
+import stat
+
+# Разрешение прав на файлы
+os.chmod(sys.argv[1], stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+os.chmod(sys.argv[1], stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
+
+# Получение аргументов
+arg1 = str(sys.argv[1])
+arg2 = str(sys.argv[2])
+
+# Проверка существования входного файла
+# if not os.path.isfile(arg1):
+#     print(f"Входной файл {arg1} не существует.")
+#     sys.exit(1)
+
+# # Проверка существования выходного файла
+# if os.path.isfile(arg2):
+#     print(f"Выходной файл {arg2} уже существует.")
+#     sys.exit(1)
+
+# Извлечение текста из PDF-файла
+text = extract_text(arg1)
+
+# Запись текста в TXT-файл
+with open(arg2, 'w', encoding='utf-8') as output_file:
+        output_file.write(text)
